2905cdb7306cd271cb47563abd9a082e9d36d0a3
janettetinoco <52670122+janettetinoco@users.noreply.github.com>
2021-05-03 20:09:29 -0700

    start leet code

---
+++ LeetCode/easy.js
@@ -0,0 +1,104 @@
+var runningSum = function (nums) {
+    let sum = [nums[0]];
+    for (let i = 1; i < nums.length; i++) {
+        sum.push(sum[sum.length - 1] + nums[i])
+    }
+    return sum
+};
+
+var defangIPaddr = function (address) {
+    let addNew = "";
+    for (let i = 0; i < address.length; i++) {
+        if (address[i] === ".") {
+            addNew += "[.]"
+        } else {
+            addNew += address[i]
+        }
+    }
+    return addNew
+};
+
+var maximumWealth = function (accounts) {
+    let max = 0;
+    accounts.forEach((customer) => {
+        let sum = customer.reduce((acc, el) => acc + el)
+        if (sum > max) max = sum;
+    })
+    return max
+};
+
+var kidsWithCandies = function (candies, extraCandies) {
+    let max = Math.max(...candies);
+    let sol = [];
+    for (let i = 0; i < candies.length; i++) {
+        if (Math.abs(candies[i] - max) <= extraCandies) {
+            sol.push(true)
+        } else {
+            sol.push(false)
+        }
+    }
+    return sol
+};
+
+var shuffle = function (nums, n) {
+    let newArr = []
+    for (let i = 0; i < n; i++) {
+        newArr.push(nums[i], nums[n + i])
+    }
+    return newArr
+};
+
+var numIdenticalPairs = function (nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) { count += 1 }
+        }
+    }
+    return count
+};
+
+var numJewelsInStones = function (jewels, stones) {
+    const jewls = {};
+    jewels.split("").map((jewl) => {
+        jewls[jewl] = 1
+    })
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+        if (jewls[stones[i]]) count += 1;
+    }
+    return count;
+};
+
+
+var ParkingSystem = function (big, medium, small) {
+    this.count = [big, medium, small]
+
+};
+
+ParkingSystem.prototype.addCar = function (carType) {
+    return this.count[carType - 1]-- > 0
+};
+
+var countNegatives = function (grid) {
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] < 0) {
+                count += 1
+            }
+        }
+    }
+    return count
+    // grid.reduce((acc, curr) => acc + curr.filter(n => n < 0).length, 0);
+};
+
+
+var smallerNumbersThanCurrent = function (nums) {
+    let sorted = nums.slice().sort((a, b) => a - b)
+    let count = []
+    for (let i = 0; i < nums.length; i++) {
+        count.push(sorted.indexOf(nums[i]))
+    }
+    return count
+};
