708a6b762091a4fa74094107c1895a406944a06a
janettetinoco <52670122+janettetinoco@users.noreply.github.com>
2021-05-11 21:40:24 -0700

    DS8

--- AlgoExpert/Medium/medium.js
+++ AlgoExpert/Medium/medium.js
@@ -87,3 +87,18 @@ function moveElementToEnd(array, toMove) {
     }
     return array
 }
+
+//Time O(n) | Space O(1)
+function moveElementToEnd(array, toMove) {
+    // Write your code here.
+    let i = 0;
+    let j = array.length - 1;
+    while (i < j) {
+        while (i < j && array[j] === toMove) j--;
+        if (array[i] === toMove) {
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        i++
+    }
+    return array
+}
